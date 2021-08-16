c771f9a4f27570fa22e50c40739ac571ff92935e
hamza <hamzahalilovic.12@gmail.com>
2021-08-16 21:51:03 +0200

    static

---
+++ build/static/js/6.0154ac62.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp-desktop"]=this["webpackJsonpapp-desktop"]||[]).push([[6],{439:function(e,t,n){"use strict";n.r(t);n(0);var s=n(3),i=n(1),c=function(e){return console.log("DATA CONSOLE PROPS ",e),Object(i.jsx)(s.Box,{width:"100vw",height:"100vh",children:Object(i.jsx)(s.Flex,{justifyContent:"center",alignItems:"center",width:"100%",height:"100%",children:Object(i.jsx)(s.Text,{textAlign:"center",textStyle:"h3",children:"DataConsole"})})})};c.displayName="DataConsole",t.default=c}}]);
+//# sourceMappingURL=6.0154ac62.chunk.js.map
